a364cf56ef82aa493ad9961f8a65d538940fc8f0
mattia971 <mattiaantonangeli@gmail.co>
2022-10-05 17:32:59 +0200

    model

---
+++ frontend/model/Location.go
@@ -0,0 +1,7 @@
+package main
+
+type Location struct {
+	City     string
+	Province string
+	Region   string
+}
---
+++ frontend/model/MountainPath.go
@@ -0,0 +1,12 @@
+package main
+
+type MountainPath struct {
+	Name       string
+	Altitude   int
+	Location   Location
+	Length     int
+	Level      string
+	Cyclable   bool
+	Family     bool
+	Historical bool
+}
