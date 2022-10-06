ef80ba5b3948c5cc6ea9eec54d985c750736f17a
mattia971 <mattiaantonangeli@gmail.co>
2022-10-06 17:16:53 +0200

    ricerca per nome

--- search/searchPath/searchPath.go
+++ search/searchPath/searchPath.go
@@ -1,17 +1,18 @@
 package searchPath
 
-import "fmt"
-
-type Location struct {
-	City     string
-	Province string
-	Region   string
-}
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
+	"log"
+)
 
 type MountainPath struct {
 	Name       string
 	Altitude   int
-	Location   Location
+	City       string
+	Province   string
+	Region     string
 	Length     int
 	Level      string
 	Cyclable   bool
@@ -26,36 +27,52 @@ type Args struct {
 type Search int
 
 func (t *Search) SimpleSearch(args *Args, reply *[]MountainPath) error {
-	fmt.Println("AO")
-	fmt.Println(*args)
-	path_1 := MountainPath{
-		Name:     "Gran Sasso",
-		Altitude: 1234,
-		Location: Location{
-			City:     "pippo",
-			Province: "Aquila",
-			Region:   "Abruzzo",
-		},
-		Length:     1094,
-		Level:      "EE",
-		Cyclable:   false,
-		Family:     false,
-		Historical: false,
+	//path_1 := MountainPath{
+	//	Name:     "Gran Sasso",
+	//	Altitude: 1234,
+	//	Location: Location{
+	//		City:     "pippo",
+	//		Province: "Aquila",
+	//		Region:   "Abruzzo",
+	//	},
+	//	Length:     1094,
+	//	Level:      "EE",
+	//	Cyclable:   false,
+	//	Family:     false,
+	//	Historical: false,
+	//}
+	//path_2 := MountainPath{
+	//	Name:     "Piccolo Sasso",
+	//	Altitude: 1234,
+	//	Location: Location{
+	//		City:     "pippo",
+	//		Province: "Aquila",
+	//		Region:   "Abruzzo",
+	//	},
+	//	Length:     1094,
+	//	Level:      "EE",
+	//	Cyclable:   false,
+	//	Family:     true,
+	//	Historical: false,
+	//}
+	var db, _ = sql.Open("sqlite3", "./search.db") // Open the created SQLite File
+	defer db.Close()                               // Defer Closing the database
+
+	var query = "SELECT * FROM Paths WHERE name LIKE " + "'%" + args.Name + "%'"
+	fmt.Println(query)
+	row, err := db.Query(query)
+	if err != nil {
+		fmt.Println("Errore query: ")
+		log.Fatal(err)
 	}
-	path_2 := MountainPath{
-		Name:     "Piccolo Sasso",
-		Altitude: 1234,
-		Location: Location{
-			City:     "pippo",
-			Province: "Aquila",
-			Region:   "Abruzzo",
-		},
-		Length:     1094,
-		Level:      "EE",
-		Cyclable:   false,
-		Family:     true,
-		Historical: false,
+	defer row.Close()
+	var path = MountainPath{}
+	for row.Next() { // Iterate and fetch the records from result cursor
+		row.Scan(&path.Name, &path.Altitude, &path.Length, &path.Level,
+			&path.Cyclable, &path.Family, &path.Historical,
+			&path.Region, &path.Province, &path.City)
+		*reply = append(*reply, path)
 	}
-	*reply = []MountainPath{path_1, path_2}
+	fmt.Println(*reply)
 	return nil
 }
