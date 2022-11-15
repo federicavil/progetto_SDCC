78a8e46e210a3f1df7498306729f115902d870f4
mattia971 <mattiaantonangeli@gmail.co>
2022-11-15 10:26:01 +0100

    fix weatherForecastServer.py

--- weatherForecast/weatherForecastServer.py
+++ weatherForecast/weatherForecastServer.py
@@ -54,15 +54,6 @@ class WeatherForecastServiceServicer(weatherForecastService_pb2_grpc.WeatherFore
         city = parsedPath["City"]
         province = parsedPath["Province"]
         region = parsedPath["Region"]
-
-        response = requests.get("https://geocoding-api.open-meteo.com/v1/search?name="+city)
-        resp = response.content.decode()
-        jsn = json.loads(resp)
-        lat=jsn["results"][0]["latitude"]
-        long=jsn["results"][0]["longitude"]
-        response = requests.get("https://api.open-meteo.com/v1/forecast?latitude="+str(lat)+"&longitude="+str(long)+"&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m")
-        resp = response.content.decode()
-        jsn = json.loads(resp)
         dict_keys = ["time","temperature","humidity","precipitation","cloud_cover","wind_speed"]
         time = []
         temperature = []
@@ -70,27 +61,38 @@ class WeatherForecastServiceServicer(weatherForecastService_pb2_grpc.WeatherFore
         precipitation = []
         cloud_cover = []
         wind_speed = []
-        #Forecasts: "{\"time\" : [\"11/11/2022\",\"12/11/2022\",\"13/11/2022\"],\"temperature\" : [4,7,12],\"cloud_cover\" : [40,100,75],\"precipitation\" : [9,10,0],\"wind_speed\" : [12,3,5],\"humidity\" : [4,8,2]}",
+        response = requests.get("https://geocoding-api.open-meteo.com/v1/search?name="+city)
+        resp = response.content.decode()
+        jsn = json.loads(resp)
+        try:
+            lat=jsn["results"][0]["latitude"]
+            long=jsn["results"][0]["longitude"]
+            response = requests.get("https://api.open-meteo.com/v1/forecast?latitude="+str(lat)+"&longitude="+str(long)+"&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m")
+            resp = response.content.decode()
+            jsn = json.loads(resp)
+            #Forecasts: "{\"time\" : [\"11/11/2022\",\"12/11/2022\",\"13/11/2022\"],\"temperature\" : [4,7,12],\"cloud_cover\" : [40,100,75],\"precipitation\" : [9,10,0],\"wind_speed\" : [12,3,5],\"humidity\" : [4,8,2]}",
+            for i in range(0,len(jsn["hourly"]["cloudcover"]),24):
+                from datetime import datetime
+                date_format = "%Y-%m-%dT%H:%M"
+                ts1 = datetime.strptime(jsn["hourly"]["time"][i], date_format)
+                date_format = "%d/%m/%Y"
+                ts2 = datetime.strftime(ts1, date_format)
+                time.append(ts2)
+                temperature.append(jsn["hourly"]["temperature_2m"][i])
+                humidity.append(jsn["hourly"]["relativehumidity_2m"][i])
+                precipitation.append(jsn["hourly"]["precipitation"][i])
+                cloud_cover.append(jsn["hourly"]["cloudcover"][i])
+                wind_speed.append(jsn["hourly"]["windspeed_10m"][i])
+        except KeyError:
+            pass
 
-        for i in range(0,len(jsn["hourly"]["cloudcover"]),24):
-            from datetime import datetime
-            date_format = "%Y-%m-%dT%H:%M"
-            ts1 = datetime.strptime(jsn["hourly"]["time"][i], date_format)
-            date_format = "%d/%m/%Y"
-            ts2 = datetime.strftime(ts1, date_format)
-            time.append(ts2)
-            temperature.append(jsn["hourly"]["temperature_2m"][i])
-            humidity.append(jsn["hourly"]["relativehumidity_2m"][i])
-            precipitation.append(jsn["hourly"]["precipitation"][i])
-            cloud_cover.append(jsn["hourly"]["cloudcover"][i])
-            wind_speed.append(jsn["hourly"]["windspeed_10m"][i])
         dict_values = [time,temperature,humidity,precipitation,cloud_cover,wind_speed]
         result_dict = {dict_keys[i]: dict_values[i] for i in range(len(dict_values))}
-
+        print(result_dict)
         #GEOCODING: https://geocoding-api.open-meteo.com/v1/search?name=Berlin
         #METEO: https://api.open-meteo.com/v1/forecast?latitude=51.5002&longitude=-0.1262&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m
 
-        ret = weatherForecastService_pb2.ForecastOutput(Forecasts=str(result_dict))
+        ret = weatherForecastService_pb2.ForecastOutput(Forecasts=str(json.dumps(result_dict)))
         return ret
 
 def serve():
